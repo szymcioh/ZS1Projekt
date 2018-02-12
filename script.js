e1f56a123b8dcd2c8afc96f26ede0cb17ecd0adc
szymcioh <szymonhorzela@gmail.com>
2018-02-12 20:16:00 +0100

    0.0.3
    Wpisywanie i sprawdzanie hasla

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function CzyJestTakiZnak(znak, ciag, haslo){
+var haslo = "";
+
+function czyJestTakiZnak(znak, ciag){
     for (i = 0; i < haslo.length; i++)
         if (haslo[i] == znak[0]){
             ciag = ciag.substr(0, i) + znak[0] + ciag.substr(i + 1);
         }
     return(ciag);
+}
+
+function wprowadzHaslo(){
+    var str = '';
+    haslo = document.getElementById("wprowadz_haslo").value;
+    document.getElementById('div wprowadz haslo').style.visibility = "hidden";
+    document.getElementById('div zgadywanie').style.visibility = "visible";
+    for (i = 0; i < haslo.length; i++)
+        str += '_';
+    document.getElementById('haslo1').innerHTML = str;
 }
