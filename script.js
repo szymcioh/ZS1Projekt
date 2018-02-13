d9cf2504aa826d9ce850722286324d0ef6fa0a07
szymcioh <szymonhorzela@gmail.com>
2018-02-13 19:10:51 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
 var haslo = "";
+var bledy = 0;
 
+$(document).ready(function(){
+    $("#password_button").click(function(){
+        var diviniation = $("#password");
+        diviniation.animate({
+            left: '-1000px',
+            height: 0,
+            width: 0
+        },400, function() {
+            diviniation.hide();
+        });
+        wprowadzHaslo();
+    });
+    
+    $(".alphabet").click(function(){
+        var diviniation = $(this);
+        if (czyJestTakiZnak(diviniation.html()))
+            diviniation.removeClass('alphabet').addClass('alphabetTrue');
+        else
+            diviniation.removeClass('alphabet').addClass('alphabetFalse');
+    });
+    
+});
 
 function czyJestTakiZnak(znak){
+    var jest = false;
     var ciag = document.getElementById("haslo1").innerHTML;
     for (i = 0; i < haslo.length; i++)
         if (haslo[i] == znak){
             ciag = ciag.substr(0, i) + znak + ciag.substr(i + 1);
+            jest = true;
         }
     document.getElementById("haslo1").innerHTML = ciag;
+    return jest;
 }
 
+
+
 function wprowadzHaslo(){
-    var str = '';
-    haslo = document.getElementById("wprowadz_haslo").value;
-    haslo = haslo.toUpperCase();
-    document.getElementById('password').style.display = "none";
-    document.getElementById('guess').style.visibility = "visible";document.getElementById('buttons').style.visibility = "visible";
-    for (i = 0; i < haslo.length; i++)
-        str += '_';
-    document.getElementById('haslo1').innerHTML = str;
+        haslo = document.getElementById("wprowadz_haslo").value;
+        haslo = haslo.toUpperCase();
+        var str = '';
+        document.getElementById('guess').style.visibility = "visible";
+        document.getElementById('buttons').style.visibility = "visible";
+        for (i = 0; i < haslo.length; i++)
+            str += '_';
+        document.getElementById('haslo1').innerHTML = str;
+
 }
 
 /*function tworzenieButtonow(){                     PORZUCONY PROJEKT FUNKCJI DO TWORZENIA PRZYCISKOW
@@ -27,4 +56,3 @@ function wprowadzHaslo(){
         document.getElementById("div przyciski").innerHTML += "<button class='przyciskiLiter' onclick='czyJestTakiZnak('" + alfabet[i] + "')'></button>"
     }
 }*/
-
